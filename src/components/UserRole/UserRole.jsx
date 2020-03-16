cb436b7daeed5e0a25ab1c353b03fc04cf5dca4c
MaksimLioshyn <leshinmaxim@gmail.com>
2020-03-16 09:35:06 +0300

    Add operations to draft copy userrole UI

--- src/components/UserRole/UserRole.jsx
+++ src/components/UserRole/UserRole.jsx
@@ -1,5 +1,6 @@
 import "./UserRole.sass";
 import React from "react";
+import { insertRow, saveCell, deleteRow } from '../common/operations';
 
 function template() {
   return (
@@ -9,4 +10,30 @@ function template() {
   );
 };
 
-export default template;
+export function onInsertRow(model) {
+    insertRow('UserRole/Post', { name: model.name })
+        .then(response => {
+            if (response.ok) {
+                alert('Added: ' + model.name)
+            }
+            throw new Error(response.statusText);
+        })
+}
+
+export function onSaveCell(value) {
+    saveCell('UserRole/Update', value)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+        }
+        )
+}
+
+export function onDeleteRow(rowKeys) {
+    rowKeys.map(key => deleteRow('UserRole/Delete', key));
+}
+
+
+export default {template, onDeleteRow, onInsertRow, onSaveCell};
+
